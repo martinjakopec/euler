1bda8056ea9482efc4ecdfa790e267ef1bfe2fb3
martinjakopec <martin.jakoopec@gmail.com>
2021-01-20 18:30:23 +0100

    syntax trim

--- eul35.py
+++ eul35.py
@@ -1,13 +1,12 @@
 from math import sqrt
 from collections import deque
+
 def is_prime(x):
-    prim = True
     for i in range(2, int(sqrt(x))+1):
         if x % i == 0:
-            prim = False
-            break
-    if prim:
-        return True
+            return False
+    return True
+        
 def is_circular(x):
     if not is_prime(x):
         return False
@@ -15,13 +14,13 @@ def is_circular(x):
     B = deque(y[:])
     L = []
     for e in range(len(B)):
-        f = "".join([str(i) for i in list(B)])
-        g = int(f)
+        g = int("".join([str(i) for i in list(B)]))        
         if is_prime(g):
             L.append(g)
             B.rotate()
     if len(L) == len(y):
         return True
+
 circular = [e for e in range(2, 1000000) if is_circular(e)]
 print(circular)
 print(len(circular))
