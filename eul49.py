e8f9fd3b7505713aca9d5329275d8e486dd4c765
martinjakopec <martin.jakoopec@gmail.com>
2021-03-17 02:56:59 +0100

    dodatak za permutacije u problemu 49

--- eul49.py
+++ eul49.py
@@ -8,7 +8,11 @@ def is_prime(x):
 for i in range(1000,10000):
     if is_prime(i):
         j = i + 3330
-    if is_prime(j):
-        k = j + 3330
-    if is_prime(k):
-        print(f"Traženi niz brojeva je {i, j, k}.")
+        if is_prime(j):
+            k = j + 3330
+            if is_prime(k):
+                a = list(str(i))
+                b = list(str(j))
+                c = list(str(k))
+                if sorted(a) == sorted(b) and sorted(b) == sorted(c):
+                    print(f"Traženi niz brojeva je {i, j, k}.")
